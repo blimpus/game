7615e53a24683cdfbc791e9c2dba92abec553934
Austin Holly <holly048@umn.edu>
2018-11-03 12:24:40 -0500

    update to spawn platforms

---
+++ Assets/Scripts/platformFall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformFall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+
+
+    void Fall() {
+        rb2d.isKinematic = false;
+    }
+
+    public float fallDelay;
+
+    private Rigidbody2D rb2d;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            Invoke("Fall", fallDelay);
+
+        }
+    }
+}
